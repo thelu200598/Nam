19009787abf2862c1f43759777616437621bce20
thelu200598 <thelu800@gmail.com>
2019-09-22 19:31:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nam/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.nam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button btnchange;
+EditText nam;
+TextView ketqua;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnchange=(Button)findViewById(R.id.btn_change);
+        nam= (EditText)findViewById(R.id.txtnam);
+        ketqua = (TextView)findViewById(R.id.txtkq);
+        btnchange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a=Integer.parseInt(nam.getText().toString());
+                String can="",chi="";
+               switch (a%10){
+                   case 0:
+                       can = "Canh";
+                       break;
+                   case 1:
+                       can = "Tân";
+                       break;
+                   case 2:
+                       can = "Nhâm";
+                       break;
+                   case 3:
+                       can = "Quý";
+                       break;
+                   case 4:
+                       can = "Giáp";
+                       break;
+                   case 5:
+                       can = "Ất";
+                       break;
+                   case 6:
+                       can = "Bính";
+                       break;
+                   case 7:
+                       can = "Đinh";
+                       break;
+                   case 8:
+                       can = "Mậu";
+                       break;
+                   case 9:
+                       can = "Kỷ";
+                       break;
+               }
+                switch (a % 12) {
+                    case 0:
+                        chi = "Thân";
+                        break;
+                    case 1:
+                        chi = "Dậu";
+                        break;
+                    case 2:
+                        chi = "Tuất";
+                        break;
+                    case 3:
+                        chi = "Hợi";
+                        break;
+                    case 4:
+                        chi = "Tý";
+                        break;
+                    case 5:
+                        chi = "Sửu";
+                        break;
+                    case 6:
+                        chi = "Dần";
+                        break;
+                    case 7:
+                        chi = "Mão";
+                        break;
+                    case 8:
+                        chi = "Thìn";
+                        break;
+                    case 9:
+                        chi = "Tỵ";
+                        break;
+                    case 10:
+                        chi = "Ngọ";
+                        break;
+                    case 11:
+                        chi = "Mùi";
+                        break;
+                }
+                ketqua.setText("Kết quả: " +can + " "+chi);
+            }
+        });
+
+    }
+}
